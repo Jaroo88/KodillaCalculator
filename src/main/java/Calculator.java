18467d57da885faa0be24374dc51718d3e575e9f
Jarosław Kołacz <jaroslaw.kolacz@op.pl>
2024-02-01 19:52:17 +0100

    Calculator

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    public static void main (String[] args) {
+        int numberOne = 12;
+        int numberTwo = 5;
+        double result = numberOne + numberTwo;
+
+        System.out.println(result);
+    }
+}
+
